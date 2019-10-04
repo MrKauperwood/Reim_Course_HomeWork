349f43d844c9fc63b8a336d714d55513680c1cd7
MrKauperwood <42598865+MrKauperwood@users.noreply.github.com>
2019-10-04 15:58:11 +0300

    Немного подправил решение

--- second.py
+++ second.py
@@ -1,11 +1,11 @@
 import math
-n = 100
+n = 2
 
 # Первое долгое решение
-k = 3
+k = 2
 spisok = []
 while k < n:
-    if k % 2 == 0:
+    if (k % 2 == 0) and (k!=2):
         k += 1
     else:
         for i in range(2, int(math.sqrt(k))+1):
@@ -17,5 +17,4 @@ while k < n:
 print(spisok)
 
 # Второе решение в одну строку
-print([y for y in range(3, n, 2) if [z for z in range(2, int(math.sqrt(y))+1) if y % z == 0] == []])
-
+print([y for y in range(2, n) if (y==2) or (y%2!=0) and [z for z in range(2, int(math.sqrt(y))+1) if y % z == 0] == []])
